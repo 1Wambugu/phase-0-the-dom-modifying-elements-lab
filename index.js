dd8da258cdacc4d0b79b986ddfc348e6d0b99307
Martin <martingakuya@gmail.com>
2023-07-03 14:20:54 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+// Remove the main#main element
+const mainElement = document.querySelector('main#main');
+mainElement.remove();
+
+// Create the newHeader variable pointing to h1#victory element
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = "Martin is the champion";
+
+// Append the newHeader element to the body
+document.body.appendChild(newHeader);
